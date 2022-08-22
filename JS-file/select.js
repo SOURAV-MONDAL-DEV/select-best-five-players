d87e4aab9310ef9db3204cee3865b8ff0c797e46
SOURAVMONDAL-BD <souravmondalcode@gmail.com>
2022-08-22 20:11:09 +0600

    select finale

--- JS-file/select.js
+++ JS-file/select.js
@@ -1,13 +1,22 @@
 
 
-function clickToSelect(nameTag){
-    let name = nameTag.innerText;
-    let createEle = document.createElement('p');
-    createEle.innerText = name;
-
+function clickToSelect(nameTag, btnTag){
     const selection = document.getElementById('selection');
-    selection.append(createEle);
-    
+    let childCount = selection.childElementCount;
+    console.log(childCount);
+
+    if(childCount < 5){
+        let name = nameTag.innerText;
+        let createEle = document.createElement('p');
+        createEle.innerText = name;
+        selection.append(createEle);
+        btnTag.disabled = true
+    }
+    else{
+        alert('You can not select more than 5 players')
+    }
 
 }
- 
+
+
+    
